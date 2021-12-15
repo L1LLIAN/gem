6899b7e6220e8c2a9cda5c0ff1858e20cfa2a762
Lillian Armes <lilyarmes@protonmail.com>
2021-12-15 16:40:40 -0500

    :sparkles: feature: Font renderer

--- src/main/java/dev/lillian/gem/module/impl/visual/HUDModule.java
+++ src/main/java/dev/lillian/gem/module/impl/visual/HUDModule.java
@@ -2,12 +2,14 @@ package dev.lillian.gem.module.impl.visual;
 
 import dev.lillian.gem.event.annotation.Subscribe;
 import dev.lillian.gem.event.events.render.RenderOverlayEvent;
+import dev.lillian.gem.font.AWTFontRenderer;
 import dev.lillian.gem.module.AbstractModule;
-import net.minecraft.client.Minecraft;
 
 import java.awt.*;
 
 public final class HUDModule extends AbstractModule {
+    private static final AWTFontRenderer VERDANA_21 = new AWTFontRenderer(new Font("Verdana", Font.PLAIN, 21), true);
+
     public HUDModule() {
         super("hud", "HUD");
         setBind(3, true); // mouse 4
@@ -15,6 +17,6 @@ public final class HUDModule extends AbstractModule {
 
     @Subscribe
     void onRenderOverlay(Class<RenderOverlayEvent> ignored) {
-        Minecraft.getMinecraft().fontRendererObj.drawString("Gem", 12, 12, Color.RED.getRGB());
+        VERDANA_21.drawString("Gem", 12, 12, Color.RED.getRGB(), false);
     }
 }
